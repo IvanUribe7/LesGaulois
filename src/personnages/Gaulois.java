7aad9ccfbdeb37c23db7cb5b47e598c167948b70
rbv4455a <rbv4455a@u3-216-10.ens.fsi>
2022-09-30 09:44:26 +0200

    creation classe Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	private int force;
+	private int effetPotion = 1;
+
+}
