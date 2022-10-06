f8cba4cc57c82176b8380b998a0d8ac2ef335cde
ivanu <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2022-10-06 12:25:22 +0200

    LesGaulois

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,9 +1,12 @@
 package personnages;
-
+import java.util.Random;
+import personnages.Gaulois;
 public class Druide {
 	private String nom;
-	private int effetPotionMin=0;
-	private int effetPotionMax=1;
+	private int effetPotionMin=5;
+	private int effetPotionMax=10;
+	private int forcePotion;
+	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
@@ -19,4 +22,32 @@ public class Druide {
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
 	}
+	public void preparerPotion() {
+		Random random = new Random();
+		forcePotion = random.nextInt(effetPotionMax-effetPotionMin)+effetPotionMin;
+		if(forcePotion>7){
+			System.out.println(prendreParole() + "« J'ai préparé une super potion de force  " + forcePotion + " »");
+		}
+		else{
+			System.out.println(prendreParole() + "« Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force " + forcePotion + " »");
+		}
+	
+	}
+	
+
+	public void booster(Gaulois gaulois) {
+		if(gaulois.getNom() == "Obelix") {
+			System.out.println(prendreParole() + "« Non, Obélix !... Tu n'auras pas de potion magique !»");
+			gaulois.parler("Par Bélénos, ce n'est pas juste !");
+		}
+		else {
+			gaulois.parler("Merci Druide, je sens que ma force est 6 fois décuplée");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+	}
+	
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,4 @@
 package personnages;
-
 public class Gaulois {
 	private String nom;
 	private int force;
@@ -21,14 +20,20 @@ public class Gaulois {
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
 				+ romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3)*effetPotion);
 	}
 	public String toString() {
-		return parler("Bonjour");
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		System.out.println(prendreParole() + " Merci Druide, je sens que ma force est 3 fois décuplée");
 	}
 	public static void main(String[] args) {
 		Gaulois asterix= new Gaulois("Asterix",8);
 		System.out.println(asterix);
+		System.out.println(asterix.effetPotion);
 
 }
 
